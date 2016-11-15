a88d297fb372093c1424238521d39e46e86a2ac8
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-11-14 16:43:55 -0800

    more tools

---
+++ .vimrc
@@ -0,0 +1,10 @@
+" minimal vim config
+
+set background=dark
+
+" search
+set incsearch
+set hlsearch
+set ignorecase
+set smartcase
+set nowrapscan
--- https-chrome.sh
+++ https-chrome.sh
@@ -11,7 +11,7 @@ GET / HTTP/1.1
 Host: $1
 Connection: keep-alive
 Cache-Control: max-age=0
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36
+User-Agent: Mozilla/5.0 AppleWebKit/537.36 Chrome/52.0.2743.116 Safari/537.36
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
 
 EOF
